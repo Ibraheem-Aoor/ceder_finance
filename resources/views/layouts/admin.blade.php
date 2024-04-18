5671f568662f001aa332f820488a4ac91840652d
Ibraheem <ibraheem.alaoor@hotmail.com>
2024-04-18 07:40:04 +0200

    update stylesheet.css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/site.css')}}?v=0.01" >
     <link rel="stylesheet" href="{{ asset('assets/css/ac.css') }}?v=0.01">
     <link rel="stylesheet" href="{{ asset('assets/css/datatables.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/stylesheet.css') }}?v=0.03">
+    <link rel="stylesheet" href="{{ asset('assets/css/stylesheet.css') }}?v=0.04">
 
     @if(env('SITE_RTL')=='on')
         <link rel="stylesheet" href="{{ asset('css/bootstrap-rtl.css') }}">
